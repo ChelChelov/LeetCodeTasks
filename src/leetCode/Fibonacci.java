3b82fb924d2683d355e491f38707ee6382a0136b
Oleg Sazonov <sazonov1994@gmail.com>
2022-07-22 19:13:24 +0300

    Add comments

--- src/leetCode/Fibonacci.java
+++ src/leetCode/Fibonacci.java
@@ -2,7 +2,7 @@ package leetCode;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibEffective(5));
+        System.out.println(fibEffective(10));
     }
 
     private static long fibNaive(int n) {
@@ -16,8 +16,11 @@ public class Fibonacci {
         long[] arr = new long[n + 1];
         arr[0] = 0;
         arr[1] = 1;
+        //Initialize first two values of array
         for (int i = 2; i <= n; i++)
+            //Get next indexes of array
             arr[i] = arr[i-1] + arr[i-2];
+        //Initialize value to arr[i] until arr[i] will not be equals arr[n]
         return arr[n];
     }
 }
